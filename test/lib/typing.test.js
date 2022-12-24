de088198a119924beff61e74af6bcbf7af866e39
octop162 <octop162@icloud.com>
2022-12-25 00:31:47 +0900

    add test

--- test/lib/typing.test.js
+++ test/lib/typing.test.js
@@ -5,6 +5,7 @@ test("Typing a alphabet character", () => {
   let result = typing.type("a");
   expect(result).toBe(true);
   expect(typing.typing).toBe("a");
+  expect(typing.remain).toBe("bc");
 });
 
 test("Typing a invalid alphabet character", () => {
@@ -12,6 +13,7 @@ test("Typing a invalid alphabet character", () => {
   let result = typing.type("z");
   expect(result).toBe(false);
   expect(typing.typing).toBe("");
+  expect(typing.remain).toBe("abc");
 });
 
 test("Complete alphabet", () => {
@@ -20,12 +22,16 @@ test("Complete alphabet", () => {
   typing.type("b");
   typing.type("c");
   expect(typing.typing).toBe("abc");
+  expect(typing.remain).toBe("");
   expect(typing.isComplete()).toBe(true);
 });
 
 test("Not Complete alphabet", () => {
   let typing = new Typing("abc");
   typing.type("a");
-  expect(typing.typing).toBe("a");
+  typing.type("z");
+  typing.type("b");
+  expect(typing.typing).toBe("ab");
+  expect(typing.remain).toBe("c");
   expect(typing.isComplete()).toBe(false);
 });
