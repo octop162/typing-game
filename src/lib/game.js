8d1103e6cee344b97c515b0fc9cf8afd4e1f12e2
octop162 <octop162@icloud.com>
2022-12-25 01:08:22 +0900

    add game

--- src/lib/game.js
+++ src/lib/game.js
@@ -1,1 +1,22 @@
-export class Game {}
+import { Typing } from "./typing";
+export class Game {
+  constructor() {
+    this.questions = [];
+  }
+
+  /**
+   * add question
+   * @param {string} question
+   */
+  addQuestion(question) {
+    this.questions.push(new Typing(question));
+  }
+
+  /**
+   * count questions
+   * @returns {number} length
+   */
+  countQuestions() {
+    return this.questions.length;
+  }
+}
--- test/lib/game.test.js
+++ test/lib/game.test.js
@@ -3,3 +3,10 @@ import { Game } from "../../src/lib/game";
 test("Create Object", () => {
   new Game();
 });
+
+test("Add Questions", () => {
+  let game = new Game();
+  game.addQuestion("ABCDE");
+  game.addQuestion("vwxyz");
+  expect(game.countQuestions()).toBe(2);
+});
