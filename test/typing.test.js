5a6a9441f04451425ccf9d8fe52bb6b7f2c3fa86
octop162 <octop162@icloud.com>
2022-12-25 00:18:40 +0900

    readme

--- test/typing.test.js
+++ test/typing.test.js
@@ -2,13 +2,8 @@ import Typing from "../lib/typing";
 
 test("Typing a alphabet character", () => {
   let typing = new Typing("abc");
-  typing.type("a");
-  expect(typing.typing).toBe("a");
-});
-
-test("Typing a japanese character", () => {
-  let typing = new Typing("あ");
-  typing.type("a");
+  let result = typing.type("a");
+  expect(result).toBe(true);
   expect(typing.typing).toBe("a");
 });
 
@@ -23,24 +18,6 @@ test("Typing alphabet complete", () => {
 
 test("Typing alphanet not complete", () => {
   let typing = new Typing("abc");
-  typing.type("a");
-  expect(typing.typing).toBe("a");
-  expect(typing.isComplete()).toBe(false);
-});
-
-test("Typing kana complete", () => {
-  let typing = new Typing("あいうえお");
-  typing.type("a");
-  typing.type("i");
-  typing.type("u");
-  typing.type("e");
-  typing.type("o");
-  expect(typing.typing).toBe("aiueo");
-  expect(typing.isComplete()).toBe(true);
-});
-
-test("Typing kana not complete", () => {
-  let typing = new Typing("あいうえお");
   typing.type("a");
   expect(typing.typing).toBe("a");
   expect(typing.isComplete()).toBe(false);
